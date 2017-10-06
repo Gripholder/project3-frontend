86949baaeef4dc83790da2c70e67b9230b8db6fc
Netsuhe <Netsuhebekele@gmail.com>
2017-10-06 14:33:13 -0400

    event handler almost done

--- src/Task.js
+++ src/Task.js
@@ -6,29 +6,46 @@ export default class Task extends Component{
   constructor(props){
     super(props)
     this.state = {
-      title: "Hello",
+      title: "",
       status: "",
       duedate: "",
       currentdate: "",
       daysleft: ""
 
-    }
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleInput = this.handleInput.bind(this);
+
+  }
 
 
 
-    render(){
-      return(
+  handleInput(e){
+    this.setState({
+
+    })
+  }
+
+
+
+  handleSubmit(e){
+    this.setState({
+
+    })
+  }
+
+  render(){
+    return(
         <div>
           <h1>Create your task and enter the date below!</h1>
         </div>
         <div>
           <form>
-            <input type="text" placeholder="Enter your task"/>
-            <input type="date" id="date" />
-            <input type="submit" value="Submit" />
+            <input type="text" placeholder="Enter your task" onInput={this.handleInput}/>
+            <input type="date" id="date" onInput={this.handleInput} />
+            <input type="submit" value="Submit" onSubmit={this.handleSbmit}/>
           </form>
         </div>
-      );
-    }
+    );
+  }
 
 }
