333a578387be22437a31a79dfcacc1f7cee563b1
Mohamed Elkarim <mohdawad2010@hotmail.com>
2017-10-06 11:56:49 -0400

    renamed the components and created component files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Person from './Person.js'
+import Task from './Task.js'
+import Home from './Home.js'
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name: "",
+      phonenum: ""
+    }
+  }
+
+
+  render() {
+    return (
+      <Router>
+        <div>
+    </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
