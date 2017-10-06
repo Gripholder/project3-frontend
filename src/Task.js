88dff98eef36e789b83b68beb3c0421655fa6ae5
Netsuhe <Netsuhebekele@gmail.com>
2017-10-06 14:38:39 -0400

    finished task

--- src/Task.js
+++ src/Task.js
@@ -14,22 +14,24 @@ export default class Task extends Component{
 
       this.handleSubmit = this.handleSubmit.bind(this);
       this.handleInput = this.handleInput.bind(this);
+      this.handleDateInput = this.handleDateInput.bind(this);
 
   }
-
-
 
   handleInput(e){
     this.setState({
-
+      title: e.target.value
     })
   }
-
-
+handleDateInput(e){
+  this.setState({
+    duedate: e.target.value
+  })
+}
 
   handleSubmit(e){
     this.setState({
-
+      
     })
   }
 
@@ -41,7 +43,7 @@ export default class Task extends Component{
         <div>
           <form>
             <input type="text" placeholder="Enter your task" onInput={this.handleInput}/>
-            <input type="date" id="date" onInput={this.handleInput} />
+            <input type="date" id="date" onInput={this.handleDateInput} />
             <input type="submit" value="Submit" onSubmit={this.handleSbmit}/>
           </form>
         </div>
