50eece83ed488a61d023e6fd0f6e032e17e5d4f0
Mohamed Elkarim <mohdawad2010@hotmail.com>
2017-10-12 14:49:38 -0400

    final modifications:

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,6 @@ import { withRouter } from 'react-router';
 import { withAuth } from './auth';
 import './App.css';
 import './css/materialize.css'
-// import './js/materialize.js'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch
-} from 'react-router-dom'
 
 import logo from './smiley-phone.png'
 import video from './phone-scroll.mp4'
@@ -23,20 +16,20 @@ export default withAuth(withRouter(props => {
     <div>
       <div>
      <header>
-     <div class="navbar-fixed">
+     <div className="navbar-fixed">
        <nav>
-     <div class="nav-wrapper">
+     <div className="nav-wrapper">
        <div id="logo">
          <img src={logo} href="/" className="App-logo brand-logo" alt="logo" />
          <h1 className="Logo-text brand-logo">iDidit</h1>
        </div>
-       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-       <ul class="right hide-on-med-and-down">
+       <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+       <ul className="right hide-on-med-and-down">
          <li><a href="sass.html">My Dashboard</a></li>
          {button}
 
        </ul>
-       <ul class="side-nav" id="mobile-demo">
+       <ul className="side-nav" id="mobile-demo">
          <li><a href="sass.html">Dashboard</a></li>
          <li><a href="badges.html">Login</a></li>
          <li><a href="collapsible.html">Logout</a></li>
@@ -61,53 +54,51 @@ export default withAuth(withRouter(props => {
    <div id="header-2">
      <h4 id="instructions-title"> How it works: </h4>
      <div id="instructions">
-       <div class="instruction-steps">
-       <i class="large material-icons">assignment</i>
+       <div className="instruction-steps">
+       <i className="large material-icons">assignment</i>
        <h5 className="steps"> 1. Create tasks </h5>
        </div>
 
-       <div class="instruction-steps">
-       <i class="large material-icons">sms_failed</i>
+       <div className="instruction-steps">
+       <i className="large material-icons">sms_failed</i>
        <h5 className="steps"> 2. Receive reminders </h5>
        </div>
 
-       <div class="instruction-steps">
-       <i class="large material-icons">done_all</i>
+       <div className="instruction-steps">
+       <i className="large material-icons">done_all</i>
        <h5 className="steps"> 3. Get stuff done </h5>
        </div>
 
      </div>
-     <a id="save-button" class="waves-effect waves-light btn-large" href="/home">I'm Ready!</a>
+     <a id="save-button" className="waves-effect waves-light btn-large" href="/home">I'm Ready!</a>
    </div>
 
-   <footer class="page-footer">
-       <div class="container">
-         <div class="row">
-           <div class="col l6 s12">
-             <h5 class="white-text">Footer Content</h5>
-             <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+   <footer className="page-footer">
+       <div className="container">
+         <div className="row">
+           <div className="col l6 s12">
+             <h5 className="white-text">Footer Content</h5>
+             <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
            </div>
-           <div class="col l4 offset-l2 s12">
-             <h5 class="white-text">Links</h5>
+           <div className="col l4 offset-l2 s12">
+             <h5 className="white-text">Links</h5>
              <ul>
-               <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-               <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-               <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-               <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+               <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+               <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+               <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+               <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
              </ul>
            </div>
          </div>
        </div>
-       <div class="footer-copyright">
-         <div class="container">
+       <div className="footer-copyright">
+         <div className="container">
          © 2014 Copyright Text
-         <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+         <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
          </div>
        </div>
      </footer>
-
  </div>
-   )
     </div>
   );
 }));
--- src/Person.js
+++ src/Person.js
@@ -3,7 +3,6 @@ import {
   Link
 } from "react-router-dom"
 import logo from './smiley-phone.png'
-import { withAuth } from './auth';
 
 import axios from 'axios'
 import './css/materialize.css'
